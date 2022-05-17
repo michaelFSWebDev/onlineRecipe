5382eba41b583c1fc38fe00a4a7d9ae9075537d9
MichaelFSWebDev <101205737+michaelFSWebDev@users.noreply.github.com>
2022-05-17 09:08:06 -0400

    wrote update veiw

--- recipeApp/views.py
+++ recipeApp/views.py
@@ -50,9 +50,20 @@ def createRecipe(request):
 
 
 
-def updateRecipe(request):
-    # return redirect('recipe:recipe-details')
-    pass
+def updateRecipe(request, recipe_id):
+    # Set the variable then use the variable to 
+    # change the post
+    recipe = Recipe_id.objects.get(id=recipe_id)
+    recipe.recipe_name = request.POST['recipe_name']
+    recipe.recipe_category = request.POST['recipe_category']
+    recipe.recipe_desc = request.POST['recipe_desc']
+    recipe.recipe_ingredient = request.POST['recipe_ingredient']
+    recipe.recipe_instruction = request.POST['recipe_instruction']
+    recipe.save()
+
+    # Redirect to details page 'app_name:named_path', named_argument 
+    return redirect('recipe:recipe-details', recipe_id)
+    
 
 def deleteRecipe(request, recipe_id):
     recipe_to_delete = Recipe_id.objects.get(id=recipe_id)
